66d2ecf9a3f48da73cc66c0683632cf503dd7856
Yoshihiro Tanaka <contact@cordea.jp>
2021-06-19 11:40:26 +0900

    initial commit :)

---
+++ example/mockito_ext_example.dart
@@ -0,0 +1,6 @@
+import 'package:mockito_ext/mockito_ext.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/mockito_ext_test.dart
@@ -0,0 +1,16 @@
+import 'package:mockito_ext/mockito_ext.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
