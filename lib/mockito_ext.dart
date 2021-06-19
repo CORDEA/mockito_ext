f758749e076b8d9fa62af50c9e3d371872a5a4e2
Yoshihiro Tanaka <contact@cordea.jp>
2021-06-19 13:23:57 +0900

    simplify

--- lib/mockito_ext.dart
+++ lib/mockito_ext.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library mockito_ext;
 
-export 'src/mockito_ext_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/mockito_ext.dart';
